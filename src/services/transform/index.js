00ef9a6dbca201990de0b0ed99a73685edfc8d93
- <>
2021-12-22 13:54:48 +0200

    export main function

--- src/services/transform/index.js
+++ src/services/transform/index.js
@@ -1,4 +1,48 @@
-/* eslint-disable no-unused-vars */
-export default function ({ fastify, self, env }) {
-    return self.transformer;
+/* eslint-disable new-cap */
+import _ from 'lodash';
+import RawInfoToChart from '../../libraries/transform/rawInfoToChart';
+import RawInfo from '../../libraries/transform/models/rawInfo.model';
+import Organisation from '../../libraries/transform/models/organisation.model';
+import tracker from '../../plugins/tracker';
+
+export default async function Transform(data) {
+    try {
+        const result = {};
+        const { rawInfos, ...extras } = data;
+        const organisation = (new Organisation(extras.organisation));
+        _.each(rawInfos, async (attrs) => {
+            const rawInfo = (new RawInfo({ ...attrs, organisation }));
+
+            let transformer;
+
+            if (rawInfo.component === 'rawInfo-chart') {
+                transformer = RawInfoToChart;
+            }
+
+            if (transformer) {
+                try {
+                    result[rawInfo.id] = (new transformer(rawInfo)).transform();
+                    result.timing = tracker.timings;
+                } catch (e) {
+                    result[rawInfo.id] = {
+                        error: true,
+                        message: e.message,
+                        name: e.name,
+                        file: `${e.fileName}:${e.lineNumber}:${e.columnNumber}`,
+                        proto: e.fileName,
+                        stack: e.stack,
+                    };
+                }
+            }
+        });
+
+        return result;
+    } catch (e) {
+        console.log('transform error', e);
+        return {
+            error: true,
+            message: e.message,
+            debug: e.stack,
+        };
+    }
 }
